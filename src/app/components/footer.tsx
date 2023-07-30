0245e0fbc911caf134ad7e7155d161338cf42a83
mattscript <ayobamifagoroye@yahoo.com>
2023-07-30 16:48:35 +0100

    work

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer(){
     return(
         
         <footer className=" text-green-500 bg-black text-base px-2 pb-2 text-center md:text-[22px] flex items-center justify-center font-black  font-exoSpace ">
-                <p>Crafted with affection and commitment to excellence. &copy; Copyright 2023. mattscript.dev All Rights Reserved.  </p>
+                <p>Crafted with affection and commitment to excellence. &copy; Copyright 2023. Mattscript.dev All Rights Reserved.  </p>
         </footer>
         
     )
